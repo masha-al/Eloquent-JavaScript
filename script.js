a0ed723c662c9456b9fb3883b2ce814a91e24e25
Dmitriy Alyokhin <mashaalyokhina@gmail.com>
2021-02-19 19:19:25 +0100

    2nd task

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let hashtag = '#';
-console.log (hashtag);
-while (hashtag.length <= 6) {
-    hashtag += '#';
-    console.log (hashtag);
+for (var i = 1; i<=100; i++) {
+  if (i%5 == 0 && i%3 == 0) {
+    console.log ("FizzBuzz")
+  }
+  else if (i%3 == 0) {
+    console.log ("Fizz")
+  }
+  else if (i%5 == 0 && i%3 != 0) {
+    console.log ("Buzz")
+  }
+  else console.log (i)
 }
