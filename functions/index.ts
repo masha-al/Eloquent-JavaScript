8726cda4535a80714cc8e4df421de60e2bdc523c
masha-al <mashaalyokhina@gmail.com>
2021-03-04 00:13:51 +0100

    Functions

---
+++ functions/index.ts
@@ -0,0 +1,28 @@
+//4
+let min = (a: number, b: number) => a < b ? a : b;
+
+console.log(min(3, 12));
+
+//5
+function isEven (number: number): number | boolean {
+  const step = number < 0 ? -2 : 2;
+  if (number === 0) return true;
+  if (number === 1 || number === -1) return false;
+
+  return isEven(number-step);
+}
+const a = isEven(-4)
+console.log(a);
+
+//6
+function countChar (sourceStr: string, char: string): number {
+  let counter = 0;
+
+  for (const letter of sourceStr) {
+    if (letter === char) counter+=1;
+  }
+
+  return counter;
+}
+
+console.log(countChar("Masha", "a"));
